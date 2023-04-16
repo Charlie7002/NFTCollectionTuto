29445171a52747a105d69205e71b3d8bcf3cfd79
chca <cachet.cha@gmail.com>
2023-04-16 14:56:57 +0200

    change contract

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// base URI =https://nft-collection-tuto.vercel.app/
 
 export default function handler(req, res) {
 	// get the tokenId from the query params
@@ -9,7 +10,7 @@ export default function handler(req, res) {
 	// The api is sending back metadata for a Crypto Dev
 	// To make our collection compatible with Opensea, we need to follow some Metadata standards
 	// when sending back the response from the api
-	// More info can be found here: https://docs.opensea.io/docs/metadata-standards
+	//but with sepolia its dead
 	res.status(200).json({
 		name: 'Crypto Dev #' + tokenId,
 		description: 'Crypto Dev is a collection of developers in crypto',
